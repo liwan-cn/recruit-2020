da5c72106fa4a09198ddb0fc4f6b0c3d65eb06ff
liwan <1349654484@qq.com>
2019-04-27 16:44:47 +0800

    fix

--- src/meituan0423/Test3/Main.java
+++ src/meituan0423/Test3/Main.java
@@ -6,8 +6,9 @@ import java.util.*;
 Solution2
  */
 
-class Solution1 {
+public class Main {
 
+    @Deprecated
     static class Rect{
         int x1, y1, x2, y2;
         Rect(int x1, int y1, int x2, int y2){
@@ -16,17 +17,9 @@ class Solution1 {
         }
     }
 
-    static class Interval{
-        int start, end;
-        Interval(int start, int end){
-            this.start = start; this.end = end;
-        }
-        @Override
-        public String toString(){
-            return "{start: " + start + ", end: " + end + "}";
-        }
-    }
+    public static final int MOD = 1000000009;
 
+    //for solution 1
     static class Line{
         int index, flag, start, end;
         Line(int index, int flag, int start, int end){
@@ -39,71 +32,62 @@ class Solution1 {
         }
     }
 
-    public static List<Interval> generateIntervals(Rect [] rects){
+    //for solution 1
+    public static List<Integer> generateXs(int [][] rects){
         List<Integer> xs = new ArrayList<>();
-        for (Rect rect : rects){
-            xs.add(rect.x1); xs.add(rect.x2);
+        for (int []rect : rects){
+            xs.add(rect[0]); xs.add(rect[2]);
         }
         Collections.sort(xs);
-        List<Interval> intervals = new ArrayList<>();
-        for (int i = 1, l = xs.size(); i < l; i++){
-            if(xs.get(i) != xs.get(i-1))
-                intervals.add(new Interval(xs.get(i-1), xs.get(i)));
-        }
-        return intervals;
+        return xs;
     }
 
-    public static List<Line> generateLines(Rect [] rects){
+    //for solution 1
+    public static List<Line> generateLines(int [][] rects){
         List<Line> lines = new ArrayList<>();
-        for (Rect rect : rects){
-            lines.add(new Line(rect.y1, 1, rect.x1, rect.x2));
-            lines.add(new Line(rect.y2, -1, rect.x1, rect.x2));
+        for (int[] rect : rects){
+            lines.add(new Line(rect[1], 1, rect[0], rect[2]));
+            lines.add(new Line(rect[3], -1, rect[0], rect[2]));
         }
         Collections.sort(lines, (o1, o2) -> (o1.index == o2.index ? o1.flag - o2.flag : o1.index - o2.index));
         return lines;
     }
 
-    public static int getArea(List<Interval> intervals, List<Line> lines){
+    //for solution 1
+    public static int getArea(List<Integer> xs, List<Line> lines){
         int res = 0;
-        for (Interval interval : intervals){
+        int preX = 0;
+        for (int nowX : xs){
             int cnt = 0;
-            int last = 0;
-            for (Line line : lines){
-                if (line.start <= interval.start && line.end >= interval.end){
-                    if (cnt > 0)
-                        res += (interval.end - interval.start) * (line.index - last);
-                    cnt += line.flag;
-                    last = line.index;
+            int lastY = 0;
+            if (nowX != preX){
+                for (Line line : lines){
+                    if (line.start <= preX && line.end >= nowX){
+                        if (cnt > 0)
+                            res = res + (nowX - preX) * (line.index - lastY);
+                        cnt += line.flag;
+                        lastY = line.index;
+                    }
                 }
-
+                preX = nowX;
             }
         }
         return res;
     }
 
-
-}
-
-/*
-Solution2
- */
-class Solution2 {
-
-    static class Rect{
-        int x1, y1, x2, y2;
-        Rect(int x1, int y1, int x2, int y2){
-            this.x1 = x1; this.y1 = y1;
-            this.x2 = x2; this.y2 = y2;
-        }
-    }
-
+    //for solution 2
     static class Interval{
         int start, end;
         Interval(int start, int end){
             this.start = start; this.end = end;
         }
+        @Override
+        public String toString(){
+            return "{start: " + start + ", end: " + end + "}";
+        }
     }
 
+    //for solution 2
     static class Node{
         int index;
         boolean isEnd;
@@ -114,17 +98,19 @@ class Solution2 {
         }
     }
 
-    public static List<Node> generateNodes(Rect [] rects){
+    //for solution 2
+    public static List<Node> generateNodes(int [][] rects){
         List<Node> nodes = new ArrayList<>();
-        for (Rect rect : rects){
-            Interval interval = new Interval(rect.y1, rect.y2);
-            nodes.add(new Node(rect.x1, false, interval));
-            nodes.add(new Node(rect.x2, true, interval));
+        for (int[] rect : rects){
+            Interval interval = new Interval(rect[1], rect[3]);
+            nodes.add(new Node(rect[0], false, interval));
+            nodes.add(new Node(rect[2], true, interval));
         }
         Collections.sort(nodes, ((o1, o2) -> (o1.index - o2.index)));
         return nodes;
     }
 
+    //for solution 2
     public static int getIntervalsLen(List<Interval> intervals){
         int len = 0;
         Collections.sort(intervals, ((o1, o2) -> (o1.start - o2.start)));
@@ -141,56 +127,52 @@ class Solution2 {
         return len;
     }
 
+    //for solution 2
     public static int getArea(List<Node> nodes){
         List<Interval> intervals = new ArrayList<>();
-        long res = 0;
-        int preIndex = 0;
+        int res = 0;
+        int preX = 0;
         for (Node node : nodes){
-            if (preIndex != node.index) {
-                res = (res + (long)getIntervalsLen(intervals) * (long)(node.index - preIndex)) % MOD;
-                preIndex = node.index;
+            if (preX != node.index) {
+                res = res + getIntervalsLen(intervals) * (node.index - preX);
+                preX = node.index;
             }
             if(node.isEnd) intervals.remove(node.interval);
             else intervals.add(node.interval);
 
         }
-        return (int)res;
+        return res;
     }
 
-    public static final int MOD = 1000000007;
-
-}
-
-public class Main{
     public static void main(String[] args) {
-
-    }
-
-    public static void test1(){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Solution1.Rect[]rects = new Solution1.Rect[n];
+        int[][] rects = new int[n][4];
         while (n -- > 0){
-            rects[n] = new Solution1.Rect(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+            rects[n][0] = sc.nextInt();
+            rects[n][1] = sc.nextInt();
+            rects[n][2] = sc.nextInt();
+            rects[n][3] = sc.nextInt();
         }
-        List<Solution1.Interval> intervals = Solution1.generateIntervals(rects);
-        List<Solution1.Line> lines = Solution1.generateLines(rects);
-        System.out.println(intervals);
+        test1(rects);
+        test2(rects);
+    }
+
+    public static void test1(int [][] rects){
+        List<Integer> xs = generateXs(rects);
+        List<Line> lines = generateLines(rects);
         System.out.println(lines);
-        System.out.println(Solution1.getArea(intervals, lines));
+        System.out.println(getArea(xs, lines));
     }
 
-    public static void test2(){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Solution2.Rect []rects = new Solution2.Rect[n];
-        while (n -- > 0){
-            rects[n] = new Solution2.Rect(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
-        }
-        List<Solution2.Node> nodes = Solution2.generateNodes(rects);
-        System.out.println(Solution2.getArea(nodes));
+    public static void test2(int [][] rects){
+        List<Node> nodes = generateNodes(rects);
+        System.out.println(getArea(nodes));
     }
 }
+
+
+
 
 /*
 3
