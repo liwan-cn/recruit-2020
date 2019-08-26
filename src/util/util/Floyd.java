13b40936599550a2990882c2842424bd3e34b841
liwan <1349654484@qq.com>
2019-08-26 13:35:27 +0800

    最短路径-弗洛伊德算法

--- src/util/util/Floyd.java
+++ src/util/util/Floyd.java
@@ -1,6 +1,16 @@
 package util.util;
 
+/**
+ * 弗洛伊德算法
+ */
+
 public class Floyd {
+
+    /**
+     * 弗洛伊德算法
+     * @param e 权重矩阵
+     * @return 最短路径的花费
+     */
     public int[][] floyd(int [][]e) {
         int n = e.length;
         for (int k = 0 ; k < n ; k ++){
