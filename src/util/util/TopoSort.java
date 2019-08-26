2958f16c68b657a12eba07d80d2654f38bdf3832
liwan <1349654484@qq.com>
2019-08-26 13:36:38 +0800

    拓扑排序

--- src/util/util/TopoSort.java
+++ src/util/util/TopoSort.java
@@ -1,13 +1,25 @@
 package util.util;
 
+/**
+ * 拓扑排序
+ */
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class TopoSort {
-    public static boolean topoSort(List<Integer>[] graph, int []inDegree, List<Integer> res){
+
+    /**
+     * 拓扑排序
+     * @param graph 图的连接graph, 每一个元素为当前节点(index下标)指向的节点集合
+     * @param inDegree 节点的入度数组
+     * @return 拓扑排序的顺序. 如果长度等于节点个数则可以完成拓扑排序
+     */
+    public static List<Integer> topoSort(List<Integer>[] graph, int []inDegree){
         Queue<Integer> queue = new LinkedList<>();
+        List<Integer> res = new ArrayList<>();
         for (int i = 0; i < inDegree.length; i++) {
             if (inDegree[i] == 0){
                 queue.offer(i);
@@ -26,12 +38,16 @@ public class TopoSort {
         }
         if (res.size() != inDegree.length){
             res.clear();
-            return false;
         }
-        return true;
-
+        return res;
     }
-    public static boolean topoSort(List<Integer>[] graph, List<Integer> res){
+
+    /**
+     * 计算各节点入度并拓扑排序
+     * @param graph 图的连接graph, 每一个元素为当前节点(index下标)指向的节点集合
+     * @return 拓扑排序的顺序. 如果长度等于节点个数则可以完成拓扑排序
+     */
+    public static List<Integer> topoSort(List<Integer>[] graph){
         //计算入度
         int []inDegree = new int[graph.length];
         for (int out = 0; out < graph.length; out++) {
@@ -39,14 +55,26 @@ public class TopoSort {
                 inDegree[in] ++;
             }
         }
-        return topoSort(graph, inDegree, res);
+        return topoSort(graph, inDegree);
     }
 
-    public static boolean topoSort(List<int []> edges, int n, List<Integer> res){
+    /**
+     * 构建图, 并完成拓扑排序
+     * @param edges 边节点列表, 每一个元素为(head, tail)
+     * @param n 节点个数
+     * @return 拓扑排序的顺序. 如果长度等于节点个数则可以完成拓扑排序
+     */
+    public static List<Integer> topoSort(List<int []> edges, int n){
         List<Integer>[] graph = TopoSort.buildGraph(edges, n);
-        return topoSort(graph, res);
+        return topoSort(graph);
     }
 
+    /**
+     * 将边节点列表转换为一个节点的所有后继节点的形式的图结构
+     * @param edges 边节点列表
+     * @param n 节点个数
+     * @return 一个节点的所有后继节点的形式的图结构形式的有向图
+     */
     public static List<Integer>[] buildGraph(List<int []> edges, int n){
         List<Integer>[] graph = new List[n];
         for (int i = 0; i < graph.length; i++) {
@@ -57,5 +85,6 @@ public class TopoSort {
         }
         return graph;
     }
+
 }
 
