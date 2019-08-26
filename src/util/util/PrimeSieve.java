8585ba2bc165e1ed03fb47ba0801a4dc4f88cda6
liwan <1349654484@qq.com>
2019-08-26 13:36:15 +0800

    素数筛-分解质因数

--- src/util/util/PrimeSieve.java
+++ src/util/util/PrimeSieve.java
@@ -1,9 +1,16 @@
 package util.util;
 
+/**
+ * 素数筛法分解质因数 eg: 12 = 2^2 * 3^1
+ */
+
 import java.util.*;
 
 public class PrimeSieve {
 
+    /**
+     * 表示因子与对应的幂指数
+     */
     class Factors{
         public int factor;
         public int power;
@@ -23,6 +30,10 @@ public class PrimeSieve {
         this(100_001);
     }
 
+    /**
+     * 初始化, 素数筛法, 寻找所有的素数
+     * @param maxSize 假设的素数最大值
+     */
     public PrimeSieve(int maxSize) {
         boolean []mark = new boolean[maxSize];
         this.prime = new ArrayList<>();
@@ -36,6 +47,11 @@ public class PrimeSieve {
         }
     }
 
+    /**
+     * 素因数分解
+     * @param n 待分解的数
+     * @return 分解后的结果 eg: 60 = 2^2 * 3^1 * 5^1
+     */
     public List<Factors> primeFactors(int n) {
         List<Factors> factors = new ArrayList<>(32);
         for (int i = 0; i < this.prime.size(); i++) {
@@ -56,11 +72,4 @@ public class PrimeSieve {
         return factors;
     }
 
-    public static void main(String[] args) {
-        PrimeSieve ps = new PrimeSieve();
-        for (int i = 100; i < 3000; i++) {
-            System.out.print(i + "\t");
-            System.out.println(ps.primeFactors(i));
-        }
-    }
 }
