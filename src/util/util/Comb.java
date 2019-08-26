bc90a424babea7484056a4121c3791052dc6f8fd
liwan <1349654484@qq.com>
2019-08-26 13:34:23 +0800

    组合数

--- src/util/util/Comb.java
+++ src/util/util/Comb.java
@@ -1,6 +1,16 @@
 package util.util;
 
+/**
+ *求组合数 C(n, m)
+ */
+
 public class Comb {
+    /**
+     * 求组合数 C(n, m), 即从 n 个不同的数中取 m 个, 共有多少种取法
+     * @param n
+     * @param m
+     * @return C(n, m)
+     */
     public static long[][] comb(int n, int m){
         long [][]dp = new long [n+1][m+1];
         for (int i = 0; i <= n; i++){
