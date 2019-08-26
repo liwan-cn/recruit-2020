50db29f8b0f73320c40b0ffd1903906302943e0a
liwan <1349654484@qq.com>
2019-08-26 13:36:26 +0800

    快速幂

--- src/util/util/QuickPower.java
+++ src/util/util/QuickPower.java
@@ -1,6 +1,17 @@
 package util.util;
 
+/**
+ * 快速幂运算
+ */
+
 public class QuickPower {
+
+    /**
+     * 快速幂运算
+     * @param x 底数
+     * @param n 幂指数
+     * @return x 的 n 次幂
+     */
     public int quickPower(int x, int n) {
         if (n == 0) return 1;
         if (n == Integer.MIN_VALUE)
